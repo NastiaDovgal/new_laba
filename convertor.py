426424beb91c5ac0f2dddc44550b1904aba404e5
NastiaDovgal <135155029+NastiaDovgal@users.noreply.github.com>
2024-10-15 03:14:17 +0300

    Create convertor.py

---
+++ convertor.py
@@ -0,0 +1,14 @@
+def usd_to_eur(amount):
+    return amount * 0.9
+
+def eur_to_usd(amount):
+    return amount * 1.08
+
+def usd_to_uah(amount):
+    return amount * 41
+
+def uah_to_usd(amount):
+    return amount * 0.024
+
+def uah_to_eur(amount):
+    return amount * 0.021
